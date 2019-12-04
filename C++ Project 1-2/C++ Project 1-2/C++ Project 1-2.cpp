2cbd6c05604aa2a78fbae87f693e17cce245f991
Ragnar Tarver <ddt.rtarver@gmail.com>
2019-12-04 20:35:26 +0900

    練習1-2 (100%)

---
+++ C++ Project 1-2/C++ Project 1-2/C++ Project 1-2.cpp
@@ -0,0 +1,20 @@
+﻿// 練習 1-2
+// List 1-1 のプログラムからusing 指令を削除して、cout をstd::cout に変更した
+// プログラムを作成せよ。
+
+
+#include <iostream>
+
+// using namespace std;
+
+int main()
+{
+	// using namespace std (standard)を使用すると
+	// stdを必要とするcout や cinはstdを使用せずに
+	// 使えますが、using namespace stdがないばあい、
+	// std::coutとして書く必要がある。
+	
+    std::cout << "初めてのC++プログラム。\n";
+	std::cout << "画面に出力しています。\n";
+
+}
