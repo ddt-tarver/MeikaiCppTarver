58bca50f5157d2140933a332df9c9d88c081bf4d
Ragnar Tarver <ddt.rtarver@gmail.com>
2019-12-04 20:15:07 +0900

    練習1-1 (100%)

---
+++ C++ Project 1-1/C++ Project 1-1/C++ Project 1-1.cpp
@@ -0,0 +1,21 @@
+﻿// 問題 1-1 
+// ヘッダ<iostream>をインクルードする指令が欠如していると、どうなるであろうか。
+// プログラムをコンパイルして検証せよ。
+
+// #include <iostream>
+
+// iostream は 入出力を行う為に必要な情報が入っているので、
+// iostreamをincludeしなかったら、coutをしようとする時にエラーが発生します。
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+	cout << "初めてのC++プログラム。 \n";
+	cout << "画面に出力しています。\n";
+
+
+}
